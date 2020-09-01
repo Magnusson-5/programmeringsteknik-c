46ada60eb8f49e5b92026ec01ed49adfc8b439aa
Magnusson <Magnusson-5@hotmail.com>
2020-09-01 14:00:14 +0200

    Created WichHolidayApp

---
+++ holiday/Holiday/WichHolidayApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WichHolidayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
